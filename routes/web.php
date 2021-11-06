e81d342846c597e66f8c1a2ee02d807825be00d1
FarhanLabib06 <17103006iubat@gmail.com>
2021-11-06 14:12:12 +0600

    master panel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.master');
 });
+Route::get('/admin',[AdminController::class,'test']);
